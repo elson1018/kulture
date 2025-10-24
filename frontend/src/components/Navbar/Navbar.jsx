1adbb169e521e252761cc2c8535c8289f585f17c
elson1018 <elsonooi2005@gmail.com>
2025-10-24 15:21:52 +0800

    Added Navbar

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import logo from '../../assets/xing.png' // will change a new icon later
+import search_icon from '../../assets/search_icon.png'
+import cart_icon from '../../assets/cart_icon.png'
+import user_icon from '../../assets/user_icon.png'
+import './Navbar.css'
+
+const Navbar = () => {
+
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    // This is the function to toggle the SidebarOpen state
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
+    return ( 
+        <>
+            <div className='navbar'>
+                <div className='navbar-logo'>
+                    <img src={logo} alt="Logo" />
+                    <p>Kulture</p>
+                </div>
+                <ul className="navbar-menu">
+                    <li>FOOD</li>
+                    <li>INSTRUMENTS</li>
+                    <li>SOUVENIORS</li>
+                    <li>DANCES</li>
+                </ul>
+                <div className="searchbar">
+                    <input type='search' placeholder='Search'/>
+                    <img src={search_icon} alt="Search Icon" />
+                </div>
+                <div className="cart">
+                    <img src={cart_icon} alt="Cart Icon" />
+                    <div className="cart-count">0</div>
+                </div>
+                <div className="user" onClick={toggleSidebar}>
+                    <img src={user_icon} alt="User Icon" />
+                </div>
+            </div>
+
+            {isSidebarOpen && (
+                <div className="sidebar-overlay" onClick={toggleSidebar}>
+                    <div className="sidebar">
+                        <img src={user_icon} alt="User Icon" />
+                        <ul>
+                            <li>My Accounts</li>
+                            <li>My Orders</li>
+                            <li>Wishlist</li>
+                            <li>Settings</li>
+                            <li>Logout</li>
+                        </ul>
+                    </div>
+                </div>
+            )}
+        </>
+    )
+}
+
+export default Navbar
