e456ae279cabfd32e7720bcd9fb051a9163ca7fe
elson1018 <elsonooi2005@gmail.com>
2025-10-27 18:41:28 +0800

    Added some components in Home Page

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -3,8 +3,36 @@ import '../CSS/Home.css'
 
 const Home = () => {
   return (
-    <div>
+    <div className='home-page'>
+      <section className="hero-section">
+        <div className="content">
+          <h1>Discover the Soul of Heritage</h1>
+          <p>placeholder for paragraph</p>
+          <button className='primary-button'>Explore Now</button>
+          <button className='secondary-button'>Start Your Journey</button>
+        </div>
+      </section>
 
+      <section className="promotion">
+        <p>Limited Time Offer:</p>
+        <h1>10% OFF all traditional instruments!</h1>
+        {/*Placeholder for photo*/}
+        <button>Shop Now</button> 
+      </section>
+
+      <section className="featured-categories">
+        <h1>Shop By Kulture</h1>
+        <div className="category-grid">
+          {/*Placeholder for category*/}
+        </div>
+      </section>
+
+      <section className="trending-products">
+        <h1>Trending in the MarketPlace</h1>
+        <div className="trending-grid">
+          {/*Placeholder for category */}
+        </div>
+      </section>
     </div>
   )
 }
