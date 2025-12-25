b31133dfce9fa9b17e490f2244287fef3fe754fd
tanchin qian <tanchinqian@gmail.com>
2025-12-25 09:39:48 +0800

    merged

--- backend/MappingServlets/src/main/java/com/example/dao/ProductDAO.java
+++ backend/MappingServlets/src/main/java/com/example/dao/ProductDAO.java
@@ -1,15 +1,11 @@
 package com.example.dao;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.example.model.Product;
 import com.example.util.MongoDBUtil;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.lang.reflect.Type;
+
 import java.util.ArrayList;
 import java.util.List;
 
